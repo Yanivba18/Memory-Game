03c8d58e1bf7647d27d1725b5841ac84d016876c
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-11-27 22:53:55 +0200

    Added start screen with different levels.

--- js/script.js
+++ js/script.js
@@ -6,22 +6,25 @@
     let score = 0;
     let wrongAnswers = 0;
     let answers = []; //Stores the answers (should never be more than 2)
+    let currentLevel;
 
     const levelEnum = {
         EASY: {
             ROW: 3,
-            COL: 4
+            COL: 4,
+            SIZE: "big"
         },
         MEDIUM: {
             ROW: 5,
-            COL: 6
+            COL: 6,
+            SIZE: "big"
         },
         HARD: {
             ROW: 7,
-            COL: 8
+            COL: 8,
+            SIZE: "small"
         }
     }
-    let currentLevel = levelEnum.EASY;
 
     function getRandomDog() {
         let dogImg;
@@ -34,20 +37,62 @@
         return dogImg;
     }
 
+    function startScreen() {
+        let mainModalSpan = $createElmnt.span();
+
+        let $h2Title = $createElmnt.h2()
+        $h2Title.append("Memory Game");
+        mainModalSpan.append($h2Title);
+
+
+        let $pTag = $createElmnt.p();
+        $pTag.append("Choose level to begin:");
+        mainModalSpan.append($pTag);
+
+        let $easyBtn = $createElmnt.button();
+        $easyBtn.append("Easy Level");
+        mainModalSpan.append($easyBtn);
+        $easyBtn.click(() => {
+            buildGrid(levelEnum.EASY);
+            modal.close();
+            currentLevel = levelEnum.EASY;
+        })
+
+        let $mediumBtn = $createElmnt.button();
+        $mediumBtn.append("Medium Level");
+        mainModalSpan.append($mediumBtn);
+        $mediumBtn.click(() => {
+            currentLevel = levelEnum.MEDIUM;
+            buildGrid(levelEnum.MEDIUM);
+            modal.close();
+        })
+
+        let $hardBtn = $createElmnt.button();
+        $hardBtn.append("Hard Level");
+        mainModalSpan.append($hardBtn);
+        $hardBtn.click(() => {
+            currentLevel = levelEnum.HARD;
+            buildGrid(levelEnum.HARD);
+            modal.close();
+        })
+
+        modal.open({ content: mainModalSpan });
+    }
+
     // ----- buildGrid gets the grid size in form of cols (columns) and rows  ----- \\
     // -----          and adds bootstrap components of row and col             ----- \\
-    function buildGrid(rows, cols) {
-
+    function buildGrid(level) {
+        updateScore();
         // ---------- Creating rows and appending them to main container div ---------- \\
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < level.ROW; i++) {
             let $rowDiv = $createElmnt.div();
-            $rowDiv.addClass("row w-50 justify-content-center");
+            $rowDiv.addClass("row w-50 justify-content-center w-100");
             $mainDiv.append($rowDiv);
 
             // ---------- Creating cols and appending them to the created row div ---------- \\
-            for (let x = 0; x < cols; x++) {
+            for (let x = 0; x < level.COL; x++) {
                 let $colDiv = $createElmnt.div();
-                $colDiv.addClass("col-sm cards m-1 text-center p-0");
+                $colDiv.addClass(`col-sm cards clickable ${level.SIZE} m-1 text-center p-0`);
                 let $innerFlipDiv = $createElmnt.div();
                 $innerFlipDiv.addClass("flip-box-inner");
                 $colDiv.append($innerFlipDiv);
@@ -62,13 +107,14 @@
                 $colDiv.click(flipImage);
             }
         }
+        fillGridImgs(level);
     }
-
-    async function fillGridImgs() {
+    // ----- filling the grid with the images from the dog API ---------- \\\\
+    async function fillGridImgs(level) {
         let $cardFront = $('.flip-box-front');
         let $cardBack = $('.flip-box-back');
         let imgsArr = [];
-        switch (currentLevel) {
+        switch (level) {
             case levelEnum.EASY:
                 imgsArr = await getImgSrcArray((levelEnum.EASY.COL * levelEnum.EASY.ROW) / 2);
                 break;
@@ -118,7 +164,7 @@
             $elmnt.children().css("transform", "");
             resetAnswers();
             console.log("Wrong answers: " + wrongAnswers)
-        }, 2000)
+        }, 1500)
     }
 
     function flip($elmnt) {
@@ -133,30 +179,39 @@
         flippedImages = 0;
     }
 
+    function updateScore() {
+        $('#wrong').text(`Tries: ${wrongAnswers}`);
+        $('#correct').text(`Score: ${score}`);
+    }
+
     function flipImage() {
         flippedImages++;
         let $this = $(this);
 
         switch (flippedImages) {
             case 1:
-                flip($this);                
+                flip($this);
 
                 console.log("less than 2, no flip back");
                 break;
 
             case 2:
-                flip($this);                
+                flip($this);
 
                 console.log("more than 2, resetting flipped, checking if match")
+                // -------- Checking if right answer or not -------- \\
                 if (checkIfMatch(answers)) {
                     answers.forEach(function (answer) {
                         answer.unbind("click", flipImage);
+                        answer.removeClass("clickable");
                     })
                     score++;
+                    updateScore();
                     resetAnswers();
                     console.log("Score: " + score);
                 } else {
                     wrongAnswers++;
+                    updateScore();
                     answers.forEach(answer => flipBack(answer));
                 };
                 break;
@@ -191,13 +246,82 @@
 
         method.div = function () { return this.createElementByTag('div') };
         method.img = function () { return this.createElementByTag('img') };
+        method.h2 = function () { return this.createElementByTag('h2') };
+        method.span = function () { return this.createElementByTag('span') };
+        method.p = function () { return this.createElementByTag('p') };
+        method.button = function () { return this.createElementByTag('button') };
         return method;
     }());
 
+    let modal = (function () {
+        let method = {},
+            $overlay,
+            $modal,
+            $content;
+
+        // Appending the modal HTML
+        $overlay = $('<div id="overlay"></div>');
+        $modal = $('<div id="modal"></div>');
+        $content = $('<div id="content"></div>');
+
+        $modal.hide();
+        $overlay.hide();
+        $modal.append($content);
+
+        $(document).ready(function () {
+            $('body').append($overlay, $modal);
+        });
+        // Center the modal in the viewport
+        method.center = function () {
+            var top, left;
+
+            top = "50%";
+            left = "50%";
+
+            $modal.css({
+                top: top,
+                left: left,
+                transform: "translateX(-50%) translateY(-50%)"
+            });
+        };
+
+        // Open the modal
+        method.open = function (settings) {
+            $content.empty().append(settings.content);
+
+            $modal.css({
+                width: settings.width || 'auto',
+                height: settings.height || 'auto'
+            })
+
+            method.center();
+
+            $(window).bind('resize.modal', method.center);
+
+            $modal.show();
+            $overlay.show();
+        };
+
+        // Close the modal
+        method.close = function () {
+            $modal.hide();
+            $overlay.hide();
+            $content.empty();
+            $(window).unbind('resize.modal');
+        };
+
+        // $close.click(function (e) {
+        //     e.preventDefault();
+        //     method.close();
+        // });
+
+        return method;
+    }());
 
     function main() {
-        buildGrid(3, 4);
-        fillGridImgs();
+        startScreen();
+        // buildGrid(3, 4);
+        // fillGridImgs();
     }
 
     main();
